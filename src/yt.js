d03be6125c5d2277ac4ac92d4728d45ae38d8d06
David Weidong Shao <weidongshao@gmail.com>
2019-10-31 08:52:57 -0700

    test

---
+++ src/yt.js
@@ -0,0 +1,60 @@
+
+var QRCode = require('qrcode')
+
+function getQRForVideos(videos, canvas) {
+    var url = 'http://weidongshao.github.io/thepoint-mandarin/yt.html?v=' + videos.join('.');
+    
+
+    console.log(url);
+    
+    QRCode.toCanvas(canvas,  url, function (error) {
+        if (error) console.error(error)
+        console.log('success!');
+    });
+}
+
+class YTManager {
+  constructor(props) {
+    this.videos = props.videos.split('.');
+    this.divid = props.divid;
+    createPlayer();
+  }
+
+  createPlayer() {
+    if (this.videos) {
+        var vid = videos[0];
+
+        player = new window.YT.Player(this.divid, {
+          height: '390',
+          width: '640',
+          videoId: vid,
+          enablejsapi: 1, 
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+    } else {
+
+    }
+  }
+
+  onPlayerReady(event) {
+        event.target.playVideo();
+  }
+  onPlayerStateChange(event) {
+    console.log(event.data);
+    if (event.data == YT.PlayerState.PLAYING) {
+      window.postMessage("play", "*");
+    } else {
+      window.postMessage("play", "*");
+      window.ReactNativeWebView.postMessage(event.data);
+    }
+  }
+
+   stopVideo() {
+     player.stopVideo();
+   }
+}
+
+export { YTManager, getQRForVideos }
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,22 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
-module.exports = {
-	entry: {
-		main: './src/app.js',
-		yt: './src/yt.js'
+module.exports = [
+	{ entry: {
+		main: './src/app.js'
 	},
     output: {
 	    filename: '[name].bundle.js',
 	    path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-	rules: [
-	    {
-		test: /\.js$/,
-		exclude: /node_modules/,
-		use: {
-		    loader: 'babel-loader',
-		    options: {
-			presets: ['@babel/preset-env']
-		    }
-		}
-	    }
-	]
-    },
-    plugins: [
-	new HtmlWebpackPlugin({
-	    title: "圣经 查询",
-	}),
-    ],
-};
+	}
+   },
+   { entry: {
+		yt: './src/yt.js',
+	},
+    output: {
+	    filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		library: 'ytlink'
+	}
+   }]
 
