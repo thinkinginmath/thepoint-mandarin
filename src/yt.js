684035fd419e23c4ccaf400276ae241afdaa0cb1
David Weidong Shao <weidongshao@gmail.com>
2019-11-02 08:41:07 -0700

    yt

--- src/yt.js
+++ src/yt.js
@@ -1,11 +1,41 @@
 
 var QRCode = require('qrcode')
 
-function getQRForVideos(videos, canvas) {
-    var url = 'http://weidongshao.github.io/thepoint-mandarin/yt.html?v=' + videos.join('.');
-    
+function initYT() {
+  window.gapi.client.setApiKey("AIzaSyBS4PMOVmYxKrJF8kiuGVALbIqGDJIi64k");
+  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+             .then(function() { console.log("GAPI client loaded for API"); },
+                   function(err) { console.error("Error loading GAPI client for API", err); });
+}
 
-    console.log(url);
+function getQRForVideos(videos, canvas, divid) {      
+    var vids=[];
+    console.log("input videos ", videos);
+    for (var url of videos) {
+      console.log("video ...", url);
+      var video_id = url.trim();
+
+      if (video_id.indexOf('v=') >0) {
+         video_id = url.split('v=')[1];
+      } else {
+         var idx = video_id.indexOf("youtu.be/");
+         if (idx > 0) {
+            video_id = video_id.substr(idx+9); 
+         }
+      }
+      console.log("vido is ", video_id);
+
+      var ampersandPosition = video_id.indexOf('&');
+      if (ampersandPosition != -1) {
+          video_id = video_id.substring(0, ampersandPosition);
+      }
+      vids.push(video_id);
+    }
+
+    var url = 'http://weidongshao.github.io/thepoint-mandarin/yt.html?v=' + vids.join('.');
+    console.log("share URL ", url);
+
+    $(divid).html('<h3>Scan this to watch videos</h3>');
     
     QRCode.toCanvas(canvas,  url, function (error) {
         if (error) console.error(error)
@@ -18,8 +48,22 @@ class YTManager {
     this.videos = props.videos.split('.');
     this.divid = props.divid;
     this.createPlayer();
+    this.getSnippets();
   }
+/*
+https://www.googleapis.com/youtube/v3/videos?key=[YOUR API KEY
+ HERE]&fields=items(snippet(title,tags,channelTitle,publishedAt),
+ statistics(viewCount))&part=snippet,statistics&id=[VIDEOID]*
+*/
 
+  getSnippets() {
+    var api_key = 'AIzaSyBS4PMOVmYxKrJF8kiuGVALbIqGDJIi64k';
+    var id_query = "id=" + this.videos.join('%2C');
+      $.ajax({url:`http://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&{id_query}&key={api_key}`})
+      .done(data => {
+console.log(data);
+      });
+    }
   createPlayer() {
     if (this.videos) {
         var vid = this.videos[0];
@@ -57,4 +101,4 @@ class YTManager {
    }
 }
 
-export { YTManager, getQRForVideos }
+export { YTManager, initYT, getQRForVideos }
